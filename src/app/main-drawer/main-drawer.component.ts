b638f9a186f058e1b825c26ecd72d988567ed573
Gabor <mgfinal@gmail.com>
2022-01-11 11:58:13 +0100

    Creating canvas element

--- src/app/main-drawer/main-drawer.component.ts
+++ src/app/main-drawer/main-drawer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-drawer',
@@ -8,10 +8,31 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AppMainDrawerComponent implements OnInit {
 
   @Input() functionEq!: string;
+  @ViewChild('canvasFunc') canvas!: ElementRef<HTMLCanvasElement>;
+
+  ctx!: CanvasRenderingContext2D;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit() {
+    const canv = this.canvas.nativeElement.getContext('2d');
+    if (!canv) {
+      throw new Error('No canvas found');
+    }
+    this.ctx = canv;
+    this.drawCoordinateSys();
+  }
+  drawCoordinateSys() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(250,500);
+    this.ctx.lineTo(250,0);
+    this.ctx.moveTo(0,250);
+    this.ctx.lineTo(500,250);
+    this.ctx.stroke();
   }
 
 }
